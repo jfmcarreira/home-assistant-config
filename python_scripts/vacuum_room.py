b58c16dca9f7beec6e72eb2bb5c5810f61d622ed
Joao Carreira <jfmcarreira@gmail.com>
2020-04-23 03:52:31 +0100

    Change file organization into recursive packages folder

--- python_scripts/vacuum_room.py
+++ python_scripts/vacuum_room.py
@@ -1,27 +1,33 @@
 room = data.get("room").lower()
-runs = int( data.get("runs") )
+runs = int( data.get("runs", '1') )
 
+room_param = []
 if room == "sala" or room == "living room":
-    room_param = 18
+    room_param.append( 18 )
 elif room == "cozinha" or room == "kitchen":
-    room_param = 19
+    room_param.append( 19 )
 elif room == "escritório" or room == "office":
-    room_param = 1
+    room_param.append( 1 )
 elif room == "quarto" or room == "bedroom":
-    room_param = 17
+    room_param.append( 17 )
 elif room == "quarto do fundo" or room == "second bedroom":
-    room_param = 21
+    room_param.append( 21 )
 elif room == "casa de banho principal" or room == "bathroom":
-    room_param = 20
+    room_param.append( 20 )
 elif room == "casa de banho do quarto" or room == "private bathroom":
-    room_param = 2
+    room_param.append( 2 )
+elif room == "quarto e cozinha" or room == "bedroom and bathroom":
+    room_param.append( 17 )
+    room_param.append( 2 )
+else:
+    exit(1)
 
 room_array = []
 if runs > 1:
     for i in range(runs):
-        room_array.append( room_param )
+        room_array.append( room_param[0] )
 else:
-    room_array = [room_param]
+    room_array = room_param
 
 service_data = { "entity_id": "vacuum.roborock", "command": "app_segment_clean", "params": room_array } 
 hass.services.call('vacuum','send_command', service_data, False)
