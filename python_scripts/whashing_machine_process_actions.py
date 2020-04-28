0598ae429912ef94d8e2872c90e38e45068df55d
Joao Carreira <jfmcarreira@gmail.com>
2020-04-28 03:08:25 +0100

    Clean notification regarding laundry

--- python_scripts/whashing_machine_process_actions.py
+++ python_scripts/whashing_machine_process_actions.py
@@ -1,19 +1,15 @@
-now = datetime.datetime.now()
-time = "{}-{}-{} {}:{}:{}.0".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
 wet_clothes = bool( hass.states.get('binary_sensor.wash_machine_wet_clothes').state )
 dry_clothes = bool( hass.states.get('binary_sensor.wash_machine_dry_clothes').state )
-logger.warning("Wet={}, Dry={}".format(wet_clothes, dry_clothes))
+#logger.warning("Wet={}, Dry={}".format(wet_clothes, dry_clothes))
 if wet_clothes:
-    logger.warning("Taking clothes out of the wahing machine at {}".format(time))
+    #logger.warning("Taking clothes out of the wahing machine at {}".format(time))
     service_data = { "chore_id": "4" }
     hass.services.call('grocy','execute_chore', service_data, False)
 
+    service_data = { "message": "clear_notification", "data" : { "tag": laundry_wet_clothes_notification}}
+    hass.services.call('notify','mobile_app_all', service_data, False)
+
 elif dry_clothes:
-    logger.warning("Taking clothes out of the clothes hanger at {}".format(time))
+    #logger.warning("Taking clothes out of the clothes hanger at {}".format(time))
     service_data = { "chore_id": "6" }
     hass.services.call('grocy','execute_chore', service_data, False)
-
-
-
-
-
