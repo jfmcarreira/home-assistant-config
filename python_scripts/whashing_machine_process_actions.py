4e242b0f55e03fd28d12a3d7e97844d29e9a408c
Joao Carreira <jfmcarreira@gmail.com>
2021-02-10 02:33:18 +0000

    Code cleanup

--- python_scripts/whashing_machine_process_actions.py
+++
@@ -1,7 +0,0 @@
-wet_clothes = bool( hass.states.get('binary_sensor.wash_machine_wet_clothes').state )
-dry_clothes = bool( hass.states.get('binary_sensor.wash_machine_dry_clothes').state )
-if wet_clothes:
-    hass.services.call('script','task_laundry_remove_wet_clothes' )
-
-elif dry_clothes:
-    hass.services.call('script','task_laundry_remove_dry_clothes' )
