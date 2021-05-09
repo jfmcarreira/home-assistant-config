57b3b79006dc7713f02af965e05621a789789b6e
Joao Carreira <jfmcarreira@gmail.com>
2021-05-10 00:41:32 +0100

    Fix bug when sending commands with broadlink

--- python_scripts/living_room_media_center_remote.py
+++ python_scripts/living_room_media_center_remote.py
@@ -20,41 +20,42 @@ if tv_source == "MEO":
     meo_box_state = hass.states.get('media_player.living_room_tv_meo_box').state
 
     broadlink_device = "meo"
-    send_via_broadlink = True
+    send_via_broadlink = False
     numeric_commands_list_meo = [ "NUMBER_1", "NUMBER_2", "NUMBER_3", "NUMBER_4", "NUMBER_5", "NUMBER_6", "NUMBER_7", "NUMBER_8", "NUMBER_9", "NUMBER_0"]
-    if button in numeric_commands_list_meo or meo_box_state == 'unavailable':
-        send_via_broadlink = True
-    else:
-        if button == "UP":
-            button = "Up"
-        elif button == "DOWN":
-            button = "Down"
-        elif button == "LEFT":
-            button = "Left"
-        elif button == "RIGHT":
-            button = "Right"
-        elif button == "HOME":
-            button = "Menu"
-        elif button == "ENTER":
-            button = "OK"
-        elif button == "BACK":
-            button = "Back"
-        elif button == "EXIT":
-            button = "Exit"
-        elif button == "INFO":
-            button = "Info"
-        elif button == "PLAY":
-            button = "PlayPause"
-        elif button == "PAUSE":
-            button = "PlayPause"
-        elif button == "SKIP-BACKWARD":
-            button = "Rewind"
-        elif button == "SKIP-FOWARD":
-            button = "Forward"
-        elif button == "PREV":
-            button = "Prev"
-        elif button == "NEXT":
-            button = "Next"
+    if not send_via_broadlink:
+        if button in numeric_commands_list_meo or meo_box_state == 'unavailable' :
+            send_via_broadlink = True
+        else:
+            if button == "UP":
+                button = "Up"
+            elif button == "DOWN":
+                button = "Down"
+            elif button == "LEFT":
+                button = "Left"
+            elif button == "RIGHT":
+                button = "Right"
+            elif button == "HOME":
+                button = "Menu"
+            elif button == "ENTER":
+                button = "OK"
+            elif button == "BACK":
+                button = "Back"
+            elif button == "EXIT":
+                button = "Exit"
+            elif button == "INFO":
+                button = "Info"
+            elif button == "PLAY":
+                button = "PlayPause"
+            elif button == "PAUSE":
+                button = "PlayPause"
+            elif button == "SKIP-BACKWARD":
+                button = "Rewind"
+            elif button == "SKIP-FOWARD":
+                button = "Forward"
+            elif button == "PREV":
+                button = "Prev"
+            elif button == "NEXT":
+                button = "Next"
 
         entity_id = "media_player.living_room_tv_meo_box" 
         service_data = { "entity_id": entity_id, "media_content_id": button, "media_content_type": "mediaroom" }
