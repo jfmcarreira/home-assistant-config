7a2534cef6ed45338556b0997dc39dc61c53a5bf
Joao Carreira <jfmcarreira@gmail.com>
2021-05-26 01:12:13 +0100

    Fix bug with the python script to reset show using mediaroom

--- python_scripts/meo_box_reset_show.py
+++ python_scripts/meo_box_reset_show.py
@@ -14,6 +14,6 @@ for i in command_list:
       service_data = { "entity_id": "remote.universal_remote_living_room_remote", "device": "meo", "command": i, "num_repeats": 1 }
       hass.services.call("remote","send_command", service_data, False)
   else:
-      service_data = { "entity_id": entity_id, "media_content_id": i, "media_content_type": "channel" }
+      service_data = { "entity_id": entity_id, "media_content_id": i, "media_content_type": "mediaroom" }
       hass.services.call('media_player','play_media', service_data, False)
 
