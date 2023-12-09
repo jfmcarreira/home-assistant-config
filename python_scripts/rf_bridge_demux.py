6ac39824a7e6bb8068a07c29fc94c795ca8b4d0c
Joao Carreira <jfmcarreira@gmail.com>
2023-12-09 02:58:51 +0000

    feat(rf): add sensor for kitchen fan

--- python_scripts/rf_bridge_demux.py
+++ python_scripts/rf_bridge_demux.py
@@ -2,7 +2,7 @@ d = { '57E10A':['washing_machine_door','ON','true'],
       '57E10E':['washing_machine_door','OFF','true'],
       '58940A':['dryer_machine_door','ON','true'],
       '58940E':['dryer_machine_door','OFF','true'],
-      'E11849':['kitchen_fan_slider','ON','true'],
+      'E11841':['kitchen_fan_slider','ON','true'],
     }
 
 p = data.get('payload')
