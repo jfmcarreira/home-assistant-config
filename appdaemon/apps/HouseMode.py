437e80027f2e34bc81424c4ea40e25b05b8e37f4
Joao Carreira <jfmcarreira@gmail.com>
2023-12-09 02:58:50 +0000

    feat(housemode): track working light to change to night mode

--- appdaemon/apps/HouseMode.py
+++ appdaemon/apps/HouseMode.py
@@ -1,35 +1,40 @@
 import hassapi as hass
 
+from enum import Enum, auto
 from datetime import datetime
 
-HOUSE_MODE_EVENT_PRESENCE       = 0
-HOUSE_MODE_EVENT_LIGHT          = 1
-HOUSE_MODE_EVENT_LIGHT_AWAKE    = 2
-HOUSE_MODE_EVENT_MOTION         = 3
-HOUSE_MODE_EVENT_NO_MOTION      = 4
-HOUSE_MODE_EVENT_NO_LIGHT       = 5
-HOUSE_MODE_EVENT_TIME           = 6
+
+class Event(Enum):
+    PRESENCE = auto()
+    WORKING_LIGHT_OFF = auto()
+    LIGHT_ON = auto()
+    MOTION = auto()
+    NO_MOTION = auto()
+    TIME = auto()
 
 
 class HouseMode(hass.Hass):
-
     def initialize(self):
+        self.house_mode = self.get_state("input_select.house_mode")
 
-        self.house_mode = self.get_state( "input_select.house_mode" )
-
-        self.trackLights = [
-            "light.living_room_group",
+        self.trackWorkingLights = [
             "light.kitchen",
             "light.office",
             "light.laundry",
             "light.hall_group",
+            "light.bathroom_rc",
+            "light.main_bathroom"
+        ]
+
+        self.trackLights = [
+            "light.living_room_group",
             "light.all_bedrooms",
             "light.all_bathrooms"
-            ]
+        ]
 
         self.trackMotion = [
-             "binary_sensor.motion_sensor_stairs",
-            ]
+            "binary_sensor.motion_sensor_stairs",
+        ]
         #     "binary_sensor.motion_sensor_kitchen",
         #     "binary_sensor.motion_sensor_living_room",
         #     "binary_sensor.motion_sensor_office",
@@ -42,19 +47,23 @@ class HouseMode(hass.Hass):
         self.listen_state(self.tracking_callback, "person.joao_carreira")
         self.listen_state(self.tracking_callback, "person.bianca_pires")
 
-        self.listen_state(self.light_callback_awake_up, "light.master_bedroom_group", new = "on", duration = 30 )
-        self.listen_state(self.light_callback_awake_up, "light.bedroom_ricardo_group", new = "on", duration = 2*60 )
-
-        self.listen_state(self.light_callback_awake_up, "light.kitchen_group", new = "on", duration = 2*60 )
-        self.listen_state(self.light_callback_awake_up, "light.living_room_group", new = "on", duration = 2*60 )
-        self.listen_state(self.light_callback_awake_up, "light.office_group", new = "on", duration = 2*60 )
-
         for entity in self.trackLights:
-            self.listen_state(self.light_callback, entity )
+            self.listen_state(self.light_callback, entity)
+
+        for entity in self.trackWorkingLights:
+            self.listen_state(self.working_light_callback, entity)
+            self.listen_state(self.light_callback_awake_up,
+                              entity, new="on", duration=120)
+
+        self.listen_state(self.light_callback_awake_up,
+                          "light.master_bedroom_group", new="on", duration=30)
+        self.listen_state(self.light_callback_awake_up,
+                          "light.bedroom_ricardo_group", new="on", duration=2*60)
+        self.listen_state(self.light_callback_awake_up,
+                          "light.living_room_group", new="on", duration=2*60)
 
         for entity in self.trackMotion:
-            self.listen_state(self.motion_callback, entity )
-
+            self.listen_state(self.motion_callback, entity)
 
         self.listen_state(self.house_mode_callback, "input_select.house_mode")
 
@@ -65,7 +74,7 @@ class HouseMode(hass.Hass):
         self.run_daily(self.update_house_mode_at_given_time, "00:05:00")
         self.run_daily(self.update_house_mode_at_given_time, "01:05:00")
 
-        self.set_new_house_mode_from_trigger( HOUSE_MODE_EVENT_TIME )
+        self.set_new_house_mode_from_trigger(Event.TIME)
 
     def house_mode_callback(self, entity, attribute, old, new, kwargs):
         self.house_mode = new
@@ -83,126 +92,131 @@ class HouseMode(hass.Hass):
 
     def is_device_on(self):
         for l in self.trackLights:
-            if self.get_state( l ) == "on":
+            if self.get_state(l) == "on":
                 return True
         for entity in self.trackState:
-            if self.get_state( entity ) == "on":
+            if self.get_state(entity) == "on":
                 return True
         return False
 
     def is_lights_on(self):
         for entity in self.trackLights:
-            if self.get_state( entity ) == "on":
+            if self.get_state(entity) == "on":
                 return True
         return False
 
     def preffered_house_mode(self):
         newMode = "On"
         if self.now_is_between("07:30:00", "08:30:00"):
-          newMode = "Evening"
+            newMode = "Evening"
         elif self.now_is_between("21:00:00", "22:00:00"):
-          newMode = "Evening"
+            newMode = "Evening"
         elif self.now_is_between("22:00:00", "08:00:00"):
-          newMode = "Night"
+            newMode = "Night"
         return newMode
 
     def is_sleep_time(self):
-      return self.now_is_between("00:00:00", "07:00:00")
+        return self.now_is_between("00:00:00", "07:00:00")
 
-    def new_house_mode_from_off(self, trigger ):
+    def new_house_mode_from_off(self, trigger):
         newMode = "Off"
         if self.anyone_home(person=True):
-          newMode = self.preffered_house_mode()
+            newMode = self.preffered_house_mode()
         return newMode
 
-    def new_house_mode_from_on(self, trigger ):
+    def new_house_mode_from_on(self, trigger):
         newMode = "On"
-        if trigger == HOUSE_MODE_EVENT_TIME:
+        if trigger == Event.TIME:
             newMode = self.preffered_house_mode()
         return newMode
 
-    def new_house_mode_from_evening(self, trigger ):
+    def new_house_mode_from_evening(self, trigger):
         newMode = "Evening"
-        if trigger == HOUSE_MODE_EVENT_LIGHT:
+        if trigger == Event.WORKING_LIGHT_OFF:
             newMode = self.preffered_house_mode()
-        elif trigger == HOUSE_MODE_EVENT_TIME:
+        elif trigger == Event.NO_MOTION:
             newMode = self.preffered_house_mode()
-        elif trigger == HOUSE_MODE_EVENT_NO_MOTION:
             if not self.is_device_on() and self.is_sleep_time():
                 newMode = "Sleep"
         return newMode
 
-    def new_house_mode_from_night(self, trigger ):
+    def new_house_mode_from_night(self, trigger):
         newMode = "Night"
-        if trigger == HOUSE_MODE_EVENT_LIGHT:
+        if trigger == Event.LIGHT_ON:
             newMode = self.preffered_house_mode()
-        elif trigger == HOUSE_MODE_EVENT_NO_MOTION:
+        elif trigger == Event.NO_MOTION:
             if not self.is_device_on() and self.is_sleep_time():
                 newMode = "Sleep"
-        elif trigger == HOUSE_MODE_EVENT_TIME:
+        elif trigger == Event.TIME:
             newMode = self.preffered_house_mode()
         return newMode
 
-    def new_house_mode_from_sleep(self, trigger ):
+    def new_house_mode_from_sleep(self, trigger):
         newMode = "Sleep"
-        if trigger == HOUSE_MODE_EVENT_LIGHT:
+        if trigger == Event.LIGHT_ON:
             newMode = self.preffered_house_mode()
         return newMode
 
-    def set_new_house_mode_from_trigger(self, trigger ):
-
+    def set_new_house_mode_from_trigger(self, trigger):
         newMode = self.house_mode
         if self.noone_home(person=True):
             newMode = "Off"
         else:
             if self.house_mode == "Off":
-                newMode = self.new_house_mode_from_off( trigger )
+                newMode = self.new_house_mode_from_off(trigger)
             elif self.house_mode == "On":
-                newMode = self.new_house_mode_from_on( trigger )
+                newMode = self.new_house_mode_from_on(trigger)
             elif self.house_mode == "Evening":
-                newMode = self.new_house_mode_from_evening( trigger )
+                newMode = self.new_house_mode_from_evening(trigger)
             elif self.house_mode == "Night":
-                newMode = self.new_house_mode_from_night( trigger )
+                newMode = self.new_house_mode_from_night(trigger)
             elif self.house_mode == "Sleep":
-                newMode = self.new_house_mode_from_sleep( trigger )
+                newMode = self.new_house_mode_from_sleep(trigger)
 
-        self.select_option("input_select.house_mode", newMode )
+        self.select_option("input_select.house_mode", newMode)
         return
 
-
     def tracking_callback(self, entity, attribute, old, new, kwargs):
-        self.set_new_house_mode_from_trigger( HOUSE_MODE_EVENT_PRESENCE )
+        self.set_new_house_mode_from_trigger(Event.PRESENCE)
 
     def motion_callback(self, entity, attribute, old, new, kwargs):
         haveMotion = False
         for entity in self.trackMotion:
-            haveMotion = haveMotion or self.get_state( entity ) == "on"
+            haveMotion = haveMotion or self.get_state(entity) == "on"
         if not haveMotion:
-            self.timer = self.run_in(self.timeout_callback, self.timer_delay() )
+            self.timer = self.run_in(self.timeout_callback, self.timer_delay())
         else:
             self.cancel_tracking_timer()
-            self.set_new_house_mode_from_trigger( HOUSE_MODE_EVENT_MOTION )
-
+            self.set_new_house_mode_from_trigger(Event.MOTION)
 
     def light_callback(self, entity, attribute, old, new, kwargs):
         isLightsOn = False
         for l in self.trackLights:
-            isLightsOn = isLightsOn or self.get_state( l ) == "on"
+            isLightsOn = isLightsOn or self.get_state(l) == "on"
         self.cancel_tracking_timer()
         if not isLightsOn:
-            self.timer = self.run_in(self.timeout_callback, self.timer_delay() )
+            self.timer = self.run_in(self.timeout_callback, self.timer_delay())
+
+    def working_light_callback(self, entity, attribute, old, new, kwargs):
+        isLightsOn = False
+        for l in self.trackWorkingLights:
+            isLightsOn = isLightsOn or self.get_state(l) == "on"
+        for l in self.trackLights:
+            isLightsOn = isLightsOn or self.get_state(l) == "on"
+        self.cancel_tracking_timer()
+        if not isLightsOn:
+            self.timer = self.run_in(self.working_light_off_callback, 60)
+
+    def working_light_off_callback(self, kwargs):
+        self.set_new_house_mode_from_trigger(Event.WORKING_LIGHT_OFF)
 
     def light_callback_awake_up(self, entity, attribute, old, new, kwargs):
         if new == "on":
-            self.set_new_house_mode_from_trigger( HOUSE_MODE_EVENT_LIGHT )
+            self.set_new_house_mode_from_trigger(Event.LIGHT_ON)
 
     def timeout_callback(self, kwargs):
-        self.set_new_house_mode_from_trigger( HOUSE_MODE_EVENT_NO_MOTION )
+        self.set_new_house_mode_from_trigger(Event.NO_MOTION)
 
     def update_house_mode_at_given_time(self, kwargs):
-        self.set_new_house_mode_from_trigger( HOUSE_MODE_EVENT_TIME )
+        self.set_new_house_mode_from_trigger(Event.TIME)
 
-
-
-
-# kate: space-indent on; indent-width 4; mixedindent off; indent-mode cstyle;
