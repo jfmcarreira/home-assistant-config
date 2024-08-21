d4bb6ca4a29556582c13fd2cbc1b2865df962fad
Joao Carreira <jfmcarreira@gmail.com>
2024-08-21 17:28:37 +0100

    feat: update living room remote for android MEO box

--- python_scripts/living_room_media_center_remote.py
+++ python_scripts/living_room_media_center_remote.py
@@ -17,54 +17,34 @@ else:
 
 if tv_source == "MEO":
 
-    meo_box_state = hass.states.get('media_player.mediaroom_82_155_7_99').state
-
-    broadlink_device = "meo"
-    send_via_broadlink = False
-    numeric_commands_list_meo = [ "NUMBER_1", "NUMBER_2", "NUMBER_3", "NUMBER_4", "NUMBER_5", "NUMBER_6", "NUMBER_7", "NUMBER_8", "NUMBER_9", "NUMBER_0"]
-    if not send_via_broadlink:
-        if button in numeric_commands_list_meo or meo_box_state == 'unavailable' :
-            send_via_broadlink = True
-        else:
-            if button == "UP":
-                button = "Up"
-            elif button == "DOWN":
-                button = "Down"
-            elif button == "LEFT":
-                button = "Left"
-            elif button == "RIGHT":
-                button = "Right"
-            elif button == "HOME":
-                button = "Menu"
-            elif button == "ENTER":
-                button = "OK"
-            elif button == "BACK":
-                button = "Back"
-            elif button == "EXIT":
-                button = "Exit"
-            elif button == "INFO":
-                button = "Info"
-            elif button == "PLAY":
-                button = "PlayPause"
-            elif button == "PAUSE":
-                button = "PlayPause"
-            elif button == "SKIP-BACKWARD":
-                button = "Rewind"
-            elif button == "SKIP-FOWARD":
-                button = "Forward"
-            elif button == "PREV":
-                button = "Prev"
-            elif button == "NEXT":
-                button = "Next"
-
-        entity_id = "media_player.mediaroom_82_155_7_99"
-        service_data = { "entity_id": entity_id, "media_content_id": button, "media_content_type": "mediaroom" }
-        hass.services.call('media_player','play_media', service_data, False)
-
-        if button == "BACK":
-            entity_id = "media_player.living_room_tv"
-            service_data = { "entity_id": entity_id, "button": button }
-            hass.services.call("webostv","button", service_data, False)
+    if button == "UP":
+        button = "DPAD_UP"
+    elif button == "DOWN":
+        button = "DPAD_DOWN"
+    elif button == "LEFT":
+        button = "DPAD_LEFT"
+    elif button == "RIGHT":
+        button = "DPAD_RIGHT"
+    elif button == "ENTER":
+        button = "DPAD_CENTER"
+    elif button == "EXIT":
+        button = "Exit"
+    elif button == "INFO":
+        button = "Info"
+    elif button == "PLAY":
+        button = "MEDIA_PLAY_PAUSE"
+    elif button == "PAUSE":
+        button = "MEDIA_PLAY_PAUSE"
+    elif button == "SKIP-BACKWARD":
+        button = "Rewind"
+    elif button == "SKIP-FOWARD":
+        button = "Forward"
+    elif button == "PREV":
+        button = "Prev"
+    elif button == "NEXT":
+        button = "Next"
+    service_data = { "entity_id": "remote.living_room_tv_meo_box", "command": button }
+    hass.services.call('remote','send_command', service_data, False)
 
 elif tv_source == 'Kodi':
 
